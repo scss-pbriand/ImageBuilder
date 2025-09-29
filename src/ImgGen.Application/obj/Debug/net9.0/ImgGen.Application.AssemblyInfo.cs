c5797727e138f92dd8b4a160434ee7bd1fb3b9db
scss-pbriand <PBriand@scview.com>
2025-09-29 11:09:23 -0400

    .

--- src/ImgGen.Application/obj/Debug/net9.0/ImgGen.Application.AssemblyInfo.cs
+++ src/ImgGen.Application/obj/Debug/net9.0/ImgGen.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ImgGen.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63d9dac6eba51490d0e06dbd117f4398c6e821cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ImgGen.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ImgGen.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
